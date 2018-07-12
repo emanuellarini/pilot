41f8d56b0c4f5c8340d61e3dc14b82942b9913a2
gmbarroso <barroso.guilherme@gmail.com>
2018-07-12 10:53:08 +0000

    recipients-add: add models

---
+++ packages/pilot/src/models/recipientLegalPartnersQuantity.js
@@ -0,0 +1,31 @@
+export default {
+  key: 'quantity',
+  name: 'quantidade de Sócios',
+  numbers: [
+    {
+      name: '0',
+      value: '0',
+    },
+    {
+      name: '1',
+      value: '1',
+    },
+    {
+      name: '2',
+      value: '2',
+    },
+    {
+      name: '3',
+      value: '3',
+    },
+    {
+      name: '4',
+      value: '4',
+    },
+    {
+      name: '5',
+      value: '5',
+    },
+  ],
+}
+
